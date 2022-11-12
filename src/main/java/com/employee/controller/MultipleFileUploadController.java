c31751fb17ebaf1c5f8edfad8a369890c08c1487
mohammadaamir1102 <mohammadaamir1102@gmail.com>
2022-11-12 22:49:56 +0530

    by java 8 multiple file upload

--- src/main/java/com/employee/controller/MultipleFileUploadController.java
+++ src/main/java/com/employee/controller/MultipleFileUploadController.java
@@ -3,6 +3,7 @@ package com.employee.controller;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,34 +19,49 @@ import com.employee.service.MultipleFileUploadService;
 @RequestMapping("/multiple-file-upload")
 public class MultipleFileUploadController {
 
-	/**
-	 * @author Mohammad Aamir
-	 */
-	
-	@Autowired
-	private MultipleFileUploadService multipleFileUploadService;
+    /**
+     * @author Mohammad Aamir
+     */
 
-	@PostMapping("/filesUpload")
-	public String uploadMultipleFile(@RequestParam("files") MultipartFile[] files) {
-		String fileName = null;
-		String fileContentType = null;
-		String sourceFileContent = null;
-		ArrayList<MultipleFileUpload> multiFileUploadList = new ArrayList<MultipleFileUpload>();
-		try {
-			for (MultipartFile multipartFile : files) {
-				fileContentType = new String(multipartFile.getBytes(), StandardCharsets.UTF_8);
-				fileName = multipartFile.getOriginalFilename();
-				sourceFileContent = new String(multipartFile.getBytes(), StandardCharsets.UTF_8);
+    @Autowired
+    private MultipleFileUploadService multipleFileUploadService;
 
-				MultipleFileUpload multipleFileUpload = new MultipleFileUpload(fileName, sourceFileContent,
-						fileContentType);
-				multiFileUploadList.add(multipleFileUpload);
-			}
-			multipleFileUploadService.saveMultipleFile(multiFileUploadList);
-			return "File Upload Successfully !!";
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "Error Occured File Uploading Time !!";
-		}
-	}
+    @PostMapping("/filesUpload")
+    public String uploadMultipleFile(@RequestParam("files") MultipartFile[] files) {
+        String fileName = null;
+        String fileContentType = null;
+        String sourceFileContent = null;
+        ArrayList<MultipleFileUpload> multiFileUploadList = new ArrayList<MultipleFileUpload>();
+        try {
+            for (MultipartFile multipartFile : files) {
+                fileContentType = new String(multipartFile.getBytes(), StandardCharsets.UTF_8);
+                fileName = multipartFile.getOriginalFilename();
+                sourceFileContent = new String(multipartFile.getBytes(), StandardCharsets.UTF_8);
+
+                MultipleFileUpload multipleFileUpload = new MultipleFileUpload(fileName, sourceFileContent, fileContentType);
+                multiFileUploadList.add(multipleFileUpload);
+            }
+
+
+//            Arrays.stream(files).forEach(file -> {
+//                try {
+//                    MultipleFileUpload multipleFileUpload = new MultipleFileUpload();
+//                    multipleFileUpload.setFileName(file.getOriginalFilename());
+//
+//                    multipleFileUpload.setContent(new String(file.getBytes()));
+//                    multipleFileUpload.setFileType(new String(file.getBytes()));
+//
+//                    multiFileUploadList.add(multipleFileUpload);
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
+//
+//            });
+            multipleFileUploadService.saveMultipleFile(multiFileUploadList);
+            return "File Upload Successfully !!";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error Occured File Uploading Time !!";
+        }
+    }
 }
