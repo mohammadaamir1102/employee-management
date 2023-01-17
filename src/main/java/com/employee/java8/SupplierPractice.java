332038e374642b9ad9d2cbdc4cf8f2afe641eccb
unknown <er.aamir1102@gmail.com>
2023-01-17 14:35:11 +0530

    java 8

---
+++ src/main/java/com/employee/java8/SupplierPractice.java
@@ -0,0 +1,28 @@
+package com.employee.java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierPractice  {
+   /* @Override
+    public String get() {
+        return "Hi Aamir khan";
+    }
+
+    public static void main(String[] args) {
+        Supplier<String> supplier = new SupplierPractice();
+        System.out.println(supplier.get());
+
+    }*/
+   public static void main(String[] args) {
+      /* Supplier<String> supplier = ()-> {
+           return "Hi Aamir khan";
+       };
+
+       System.out.println(supplier.get());*/
+       List<String> letters = Arrays.asList("a","b");
+       String value = letters.stream().findAny().orElseGet(() -> "Empty Data");
+       System.out.println(value);
+   }
+}
