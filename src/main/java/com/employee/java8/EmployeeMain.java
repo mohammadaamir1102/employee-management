b9e461af81246e015ed3ed0defe083192e73585e
unknown <er.aamir1102@gmail.com>
2023-01-19 14:54:32 +0530

    list sorting

--- src/main/java/com/employee/java8/EmployeeMain.java
+++ src/main/java/com/employee/java8/EmployeeMain.java
@@ -8,9 +8,9 @@ public class EmployeeMain {
 
     public static List<Employee> findEmployees() {
         List<Employee> employees = Arrays.asList(
-                new Employee(1, "Aamir khan", 10000.0),
+                new Employee(3, "Aamir khan", 10000.0),
                 new Employee(2, "Imran khan", 20000.0),
-                new Employee(3, "Muzammil khan", 30000.0),
+                new Employee(1, "Muzammil khan", 30000.0),
                 new Employee(4, "Noman khan", 40000.0)
         );
         return employees;
---
+++ src/main/java/com/employee/java8/SortingListUsingLambda.java
@@ -0,0 +1,37 @@
+package com.employee.java8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortingListUsingLambda {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Collections.addAll(list, 5, 3, 4, 6, 7, 9, 2, 10, 1, 8);
+
+        System.out.println("----------------");
+        Collections.sort(list);
+        System.out.println(list); // Ascending
+
+        System.out.println("----------------");
+        Collections.reverse(list); //Descending
+        System.out.println(list);
+
+        System.out.println("_________________");
+        list.stream().sorted().forEach(System.out::println); //Ascending
+
+        System.out.println("_________________");
+        list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println); //Descending
+
+        System.out.println("_________________");
+        List<Employee> employees = EmployeeMain.findEmployees();
+        Collections.sort(employees, (e1, e2) -> e2.getName().compareTo(e1.getName()));
+        employees.forEach(System.out::println);
+
+        System.out.println("___________________");
+        employees.stream().sorted(Comparator.comparing(empName->empName.getName())).forEach(System.out::println);
+
+    }
+}
