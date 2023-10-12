893fa38945d9dc1930436a8c051a2e1183fdeab9
mohammadaamir1102 <mohammadaamir1102@gmail.com>
2023-10-12 21:04:22 +0530

    RemoveBraces

--- src/main/java/com/employee/java8/Test.java
+++ src/main/java/com/employee/java8/Test.java
@@ -1,66 +1,21 @@
 package com.employee.java8;
 
 
-import com.employee.java8.interview.EmployeeBean;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test {
     public static void main(String[] args) {
+        String s1 = "[Hello]Ignore[World]";
+        Pattern pattern = Pattern.compile("\\[(.*?)\\]");
+        Matcher matcher = pattern.matcher(s1);
+        StringBuilder result = new StringBuilder();
 
-     /*
-     *
-1|Ryan|M|100|Mumbai|1
-2|Joanna|F|150|MUMBAI|2
-Srno|Name|Gender|score|location|DepartmentID
-3|Om|F|190|Pune|4
-4|Krishna|M|180|Delhi|4
-5|Ram|M|200|Jaipur|4
-6|Radha|F|90|Indore|5
-7|Om|M|80|Jaipur|5
-8|Sita|F|125|Raipur|3
-     * */
-
-        String[] employeeArray = {"1|Ryan|M|100|Mumbai|1", "2|Joanna|F|150|MUMBAI|2"};
-
-        List<EmployeeBean> employees = convertStringValueToObject(employeeArray);
-        employees.forEach(System.out::println);
-    }
-
-
-    private static List<EmployeeBean> convertStringValueToObject(String[] employeeArray) {
-        List<EmployeeBean> employeeList = new ArrayList<>();
-        for (String value : employeeArray) {
-            String[] parse = value.split("\\|");
-            if (parse.length == 6) {
-
-                EmployeeBean employeeBean = getEmployeeBean(parse);
-
-                employeeList.add(employeeBean);
-            }
+        while (matcher.find()) {
+            result.append(matcher.group(1)).append(" ");
         }
-        return employeeList;
+
+        String finalResult = result.toString().trim();
+        System.out.println(finalResult);
     }
-
-    private static EmployeeBean getEmployeeBean(String[] parse) {
-        String id = parse[0].trim();
-        String name = parse[1].trim();
-        String gender = parse[2].trim();
-        String score = parse[3].trim();
-        String location = parse[4].trim();
-        String departmentId = parse[5].trim();
-
-        //set the data to entity
-        EmployeeBean employeeBean = new EmployeeBean();
-        employeeBean.setSrno(id);
-        employeeBean.setName(name);
-        employeeBean.setGender(gender);
-        employeeBean.setScore(score);
-        employeeBean.setLocateion(location);
-        employeeBean.setDepartmentId(departmentId);
-        return employeeBean;
-    }
-
-
 }
---
+++ src/main/java/com/employee/java8/interview/RemoveBraces.java
@@ -0,0 +1,32 @@
+package com.employee.java8.interview;
+
+public class RemoveBraces {
+
+    public static void main(String[] args) {
+        /*
+         * String s1 = "[Hello]Ignore[World]";
+         * out put = Hello World
+         */
+
+        String input = "[Hello]Ignore[World]";
+        String output = removeBraces(input);
+        System.out.println(output);
+
+
+    }
+
+    private static String removeBraces(String input) {
+        StringBuilder output = new StringBuilder();
+        boolean insideTheBraces = false;
+        for (char c : input.toCharArray()) {
+            if (c == '[') {
+                insideTheBraces = true;
+            } else if (c == ']') {
+                insideTheBraces = false;
+            } else if (insideTheBraces) {
+                output.append(c);
+            }
+        }
+        return output.toString();
+    }
+}
