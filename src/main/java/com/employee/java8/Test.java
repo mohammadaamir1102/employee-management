d2c39fd2520ac03babff2f00fd03c685091e2cea
mohammadaamir1102 <mohammadaamir1102@gmail.com>
2023-11-29 19:05:50 +0530

    some changes

--- src/main/java/com/employee/java8/Test.java
+++ src/main/java/com/employee/java8/Test.java
@@ -1,17 +1,41 @@
 package com.employee.java8;
 
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        String[] inputArray = {"apple", "banana", "cherry", "apple", "date",
-                "banana", "elderberry", "fig", "cherry"};
-
-
+        Integer month = null;
+        String input = "jan";
+        month = switch (input){
+            case "jan" -> months(input);
+            case "feb" -> months(input);
+            default -> throw new IllegalStateException("Unexpected value: " + input);
+        };
+        System.out.println(month);
     }
+
+    public static String janMethod() {
+        return "Jan";
+    }
+
+    public static String febMethod() {
+        return "feb";
+    }
+
+    public static String marchMethod() {
+        return "march";
+    }
+
+    public static String aprilMethod() {
+        return "april";
+    }
+
+    public static Integer months(String month) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("jan", 1);
+        map.put("feb", 2);
+        return (map.containsKey(month.toLowerCase())) ? map.get(month) : null;
+    }
+
 }
